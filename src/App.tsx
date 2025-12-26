30c888524190afdeb3732483d82aca25ca2e8170
sudarsanyes <sudarsanyes@gmail.com>
2025-12-26 18:35:07 +0100

    More UI tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-shell">
       <header className="app-header">
         <img src={logo} alt="Thea's Cookies & Cakes Logo" className="logo" />
         <h3>Thea's Cookies & Cakes</h3>
       </header>
-      <main className="page">
+      <main>
         <InventoryPage />
       </main>
     </div>
